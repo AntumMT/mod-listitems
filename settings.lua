8171f1fb543ed583c6559f27c5963c8489874b76
AntumDeluge <antumdeluge@gmail.com>
2017-08-03 16:54:09 -0700

    Move config settings into 'settings.lua'

---
+++ settings.lua
@@ -0,0 +1,13 @@
+--[[ LICENSE HEADER
+  
+  MIT Licensing
+  
+  Copyright © 2017 Jordan Irwin
+  
+  See: LICENSE.txt
+--]]
+
+--- @script settings
+
+
+listitems.debug = core.settings:get_bool('enable_debug_mods') or false
